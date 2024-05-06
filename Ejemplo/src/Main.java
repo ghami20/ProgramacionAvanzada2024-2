3f49214d8385dec9b81bdfe3192ecfa8e1e4aa46
ghami20 <ghamiboxbox@gmail.com>
2024-05-06 11:57:11 -0300

    Programa de notas

--- Ejemplo/src/Main.java
+++ Ejemplo/src/Main.java
@@ -2,6 +2,14 @@ import javax.swing.JOptionPane;
 
 public class Main {
 public static void main(String[] args) {
+	
 	JOptionPane.showMessageDialog(null, "HOla");
+	
+	int nota=10;
+	if (nota>7) {
+		
+	} else {
+
+	}
 }
 }
